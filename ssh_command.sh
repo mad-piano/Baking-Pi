a7df0a247116b7c88abb263cd1b84c8b6ddda332
mad-piano <68827682+mad-piano@users.noreply.github.com>
2024-04-01 19:55:37 -0400

    Update ssh_command.sh

--- ssh_command.sh
+++ ssh_command.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Array of IP addresses of all Raspberry Pis in the cluster, including the host
-pi_ips=("169.254.116.109" "169.254.38.164")
+# Array of IP addresses and corresponding hostnames of all Raspberry Pis in the cluster, including the host
+pi_info=("192.168.1.100 bevmic2" "192.168.1.101 bevmic")
 
 # SSH into each Raspberry Pi and execute a command
-for ip in "${pi_ips[@]}"
+for info in "${pi_info[@]}"
 do
-    echo "Connecting to Raspberry Pi at $ip"
-    ssh pi@$ip 'for i in $(seq 0 3); do echo "Hello World from core $i"; done'
+    ip=$(echo $info | cut -d' ' -f1)
+    hostname=$(echo $info | cut -d' ' -f2)
+    echo "Connecting to Raspberry Pi at $hostname ($ip)"
+    ssh $hostname@$ip 'for i in $(seq 0 3); do echo "Hello World from core $i"; done'
 done
